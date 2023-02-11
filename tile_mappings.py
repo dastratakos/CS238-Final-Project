4d2624dfdeea9a5d1f9036ed9b2fcc2e3503f848
Dean Stratakos <dastratakos@gmail.com>
2023-02-11 02:11:42 -0800

    Load in maps

---
+++ tile_mappings.py
@@ -0,0 +1,47 @@
+tile_mappings = {
+    0: "Air",
+    1: "Square",
+    2: "Spike",
+    3: "ShortSpike0",
+    4: "GridSquare0",
+    5: "GridSquare1",
+    6: "GridSquare2",
+    7: "GridSquare3",
+    8: "GridSquare4",
+    9: "GridSquare5",
+    10: "GridSquare6",
+    11: "GridSquare7",
+    12: "GridSquare8",
+    13: "GridSquare9",
+    14: "GridSquare10",
+    15: "GridSquare11",
+    16: "GridSquare12",
+    17: "GridSquare13",
+    18: "GridSquare14",
+    19: "GridSquare15",
+    20: "PlatformTop",
+    21: "SpikeGround0",
+    22: "SpikeGround1",
+    23: "SpikeDown",
+    24: "ChainBottom",
+    25: "ChainMiddle",
+    26: "ChainTop",
+    27: "PlatformTopSpike",
+    28: "GridSquare16",
+    29: "GridSquare17",
+    30: "GridSquare18",
+    31: "PortalEntrance", #
+    32: "PortalExit", #
+    33: "JumpPad",
+    34: "JumpOrb", #
+    35: "PortalInvert", #
+    36: "PortalRevert", #
+    37: "GridSquare19", #
+    38: "SpikeLeft",
+    39: "SpikeRight",
+    40: "ShortSpikeDown",
+    41: "SpikeGround0Down",
+    42: "SpikeGround1Down",
+    43: "PlatformBottom",
+    44: "End",
+}
